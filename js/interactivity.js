23c5009a01c50dd3d0d01be9c65cc59156db6f8d
Steven <Steven>
2016-08-10 11:37:16 +0100

    latest update 1470825436601

--- js/interactivity.js
+++
@@ -1,6 +0,0 @@
-jQuery("#credits").on("click", function() {
-    var message = "Game created by the one and only, your boi Stern";
-    jQuery("#credits").append(
-        "<p>" + message + "</p>"
-    );
-});
